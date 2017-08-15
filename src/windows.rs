cda27cd8afeed4ea99acf6532fb1e4d109f39bb1
mark.rossington <no>
2017-08-15 14:29:18 +0100

    Fix bit check logic for the connection setting byte
    Also Add some data about the byte

--- src/windows.rs
+++ src/windows.rs
@@ -16,11 +16,23 @@ enum AutoconfigType {
 
 fn proxy_autoconfig_type() -> AutoconfigType {
     if let Ok(key) = RegKey::predef(HKEY_CURRENT_USER).open_subkey_with_flags(REG_CONNECTIONS, KEY_READ) {
+        // Format of DefaultConnectionSettings is a string of bytes
+        // Only interested in byte 9 here which values mean:
+        //  09 when only 'Automatically detect settings' is enabled 
+        //  03 when only 'Use a proxy server for your LAN' is enabled
+        //  0B when both are enabled
+        //  05 when only 'Use automatic configuration script' is enabled
+        //  0D when 'Automatically detect settings' and 'Use automatic configuration script' are enabled
+        //  07 when 'Use a proxy server for your LAN' and 'Use automatic configuration script' are enabled
+        //  0F when all the three are enabled. 
+        //  01 when none of them are enabled. 
+        // Source https://superuser.com/questions/419696/in-windows-7-how-to-change-proxy-settings-from-command-line
+        
         match key.get_raw_value("DefaultConnectionSettings") {
             Ok(RegValue { ref bytes, .. }) if bytes.len() > 8 => {
-                if (bytes[8] & (1 << 2)) == bytes[8] {
+                if (bytes[8] & (1 << 2)) == (1 << 2) {
                     return AutoconfigType::Pac;
-                } else if (bytes[8] & (1 << 3)) == bytes[8] {
+                } else if (bytes[8] & (1 << 3)) == (1 << 3) {
                     return AutoconfigType::Wpad;
                 }
             },
