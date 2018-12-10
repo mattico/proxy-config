0eb96e1fcbcc0de5852387fd962bb130f8ac5b2e
Ion Banaru <banaruion@yahoo.ca>
2018-12-10 14:09:45 -0500

    add test for macos

--- src/macos.rs
+++ src/macos.rs
@@ -5,8 +5,10 @@ use std::{
     fs::File,
     collections::HashMap,
 };
+use std::borrow::Borrow;
 
 use super::*;
+
 #[derive(Debug)]
 pub struct ProxyConfigEx {
     pub proxy: Url,
@@ -16,10 +18,22 @@ pub struct ProxyConfigEx {
     pub whitelist: String,
 }
 
-pub fn get_proxy_config_ex() -> Result<Vec<ProxyConfigEx>> {
+pub trait ProxyConfigReader {
+    fn read_proxy_config(&self) -> Option<Plist>;
+}
 
-    let plist = File::open("/Library/Preferences/SystemConfiguration/preferences.plist").ok()
-        .and_then(|file| Plist::read(file).ok()).ok_or(OsError)?;
+pub struct Reader {}
+
+impl ProxyConfigReader for Reader {
+    fn read_proxy_config(&self) -> Option<Plist> {
+        File::open("/Library/Preferences/SystemConfiguration/preferences.plist").ok()
+            .and_then(|file| Plist::read(file).ok())
+    }
+}
+
+pub fn get_proxy_config_ex(reader: &ProxyConfigReader) -> Result<Vec<ProxyConfigEx>> {
+
+    let plist = reader.read_proxy_config().ok_or(OsError)?;
 
     if let Some(Plist::Dictionary(network_services)) = plist.as_dictionary()
         .and_then(|decoded_data| decoded_data.get("NetworkServices")) {
@@ -34,7 +48,7 @@ pub fn get_proxy_config_ex() -> Result<Vec<ProxyConfigEx>> {
                 .as_dictionary().ok_or(InvalidConfigError)?;
 
             for entry in proxy.keys() {
-                if entry.contains("Proxy") {
+                if entry.ends_with("Proxy") {
                     // Ex: entry = "HTTPSProxy".
                     let protocol = entry.replace("Proxy","");
                     let scheme;
@@ -103,7 +117,9 @@ pub fn get_proxy_config_ex() -> Result<Vec<ProxyConfigEx>> {
 }
 
 pub(crate) fn get_proxy_config() -> Result<ProxyConfig> {
-    let proxy_configs = get_proxy_config_ex()?;
+    let reader: Box<ProxyConfigReader> = Box::new(Reader{});
+    let proxy_configs = get_proxy_config_ex(reader.borrow())?;
+
     let mut proxies = HashMap::new();
     let mut whitelist = Vec::new();
     for proxy_config in proxy_configs {
@@ -118,4 +134,27 @@ pub(crate) fn get_proxy_config() -> Result<ProxyConfig> {
         whitelist,
         ..Default::default()
     });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    pub struct Test {}
+    impl ProxyConfigReader for Test {
+        fn read_proxy_config(&self) -> Option<Plist> {
+            File::open("src/test_data/preferences.plist").ok()
+                .and_then(|file| Plist::read(file).ok())
+        }
+    }
+
+    #[test]
+    fn test_os_version () {
+        let reader: Box<ProxyConfigReader> = Box::new(Test{});
+        let proxy_configs = &get_proxy_config_ex(reader.borrow()).unwrap()[0];
+        assert_eq!(proxy_configs.proxy, Url::parse("https://127.0.0.1:50001/").unwrap());
+        assert_eq!(proxy_configs.port, 50001);
+        assert_eq!(proxy_configs.protocol, "https");
+        assert_eq!(proxy_configs.interface, "Thunderbolt Bridge");
+        assert_eq!(proxy_configs.whitelist, r#"["*.local","169.254/16","123.0.0.1/15"]"#);
+    }
 }
